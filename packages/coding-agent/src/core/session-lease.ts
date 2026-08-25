I5858555255/Prime-Agent-Novel-Engine

fix(coding-agent): stabilize macOS process identity
Run the portable ps start-time query under a fixed timezone and locale so a system timezone change does not invalidate daemon ownership metadata.\n\nFixes #879

packages/coding-agent/src/core/session-lease.ts
@@ -110,12 +110,17 @@ function isProcessAlive(pid: number): boolean {
 	}
 }
 
-type ProcessQuery = (command: string, args: string[]) => string;
+interface ProcessQueryOptions {
+	env?: NodeJS.ProcessEnv;
+}
 
-function runProcessQuery(command: string, args: string[]): string {
+type ProcessQuery = (command: string, args: string[], options?: ProcessQueryOptions) => string;
+
+function runProcessQuery(command: string, args: string[], options?: ProcessQueryOptions): string {
 	return execFileSync(command, args, {
 		encoding: "utf8",
 		stdio: ["ignore", "pipe", "ignore"],
+		env: options?.env,
 	});
 }
 
@@ -132,6 +137,20 @@ export function getWindowsProcessStartId(pid: number, query: ProcessQuery = runP
 			`([System.Diagnostics.Process]::GetProcessById(${pid})).StartTime.ToUniversalTime().Ticks`,
 		]).trim();
 		return /^\d+$/.test(startTicks) ? `win:${startTicks}` : undefined;
+	} catch {
+		return undefined;
+	}
+}
+
+export function getPsProcessStartId(pid: number, query: ProcessQuery = runProcessQuery): string | undefined {
+	if (!Number.isInteger(pid) || pid <= 0) {
+		return undefined;
+	}
+	try {
+		const startTime = query("ps", ["-p", String(pid), "-o", "lstart="], {
+			env: { ...process.env, TZ: "UTC", LC_ALL: "C" },
+		}).trim();
+		return startTime ? `ps:${startTime}` : undefined;
 	} catch {
 		return undefined;
 	}
@@ -155,12 +174,7 @@ export function getProcessStartId(pid: number): string | undefined {
 	} catch {
 		// Fall through to the portable process listing used on macOS and BSD.
 	}
-	try {
-		const startTime = runProcessQuery("ps", ["-p", String(pid), "-o", "lstart="]).trim();
-		return startTime ? `ps:${startTime}` : undefined;
-	} catch {
-		return undefined;
-	}
+	return getPsProcessStartId(pid);
 }
 
 let currentProcessStartId: string | undefined;

packages/coding-agent/test/daemon-ps.test.ts
@@ -15,7 +15,7 @@ import {
 	sortDaemons,
 	verifyHelloSupervisorPid,
 } from "../src/cli/daemon-ps.js";
-import { getProcessStartId } from "../src/core/session-lease.js";
+import { getProcessStartId, getPsProcessStartId } from "../src/core/session-lease.js";
 import { defaultDaemonSocketDir } from "../src/modes/daemon/daemon-socket.js";
 
 describe("worker socket classification", () => {
@@ -112,6 +112,38 @@ describe("verifyHelloSupervisorPid", () => {
 		expect(verifyHelloSupervisorPid(process.pid, processStartId)).toBe(process.pid);
 		if (processStartId) {
 			expect(verifyHelloSupervisorPid(process.pid, `${processStartId}-stale`)).toBeUndefined();
+		}
+	});
+});
+
+describe("getPsProcessStartId", () => {
+	it("uses a stable timezone and locale for the portable process identity", () => {
+		let queryEnvironment: NodeJS.ProcessEnv | undefined;
+		const result = getPsProcessStartId(1234, (_command, _args, options) => {
+			queryEnvironment = options?.env;
+			return "Sat Aug  8 06:00:00 2026\n";
+		});
+
+		expect(result).toBe("ps:Sat Aug  8 06:00:00 2026");
+		expect(queryEnvironment).toMatchObject({ TZ: "UTC", LC_ALL: "C" });
+	});
+
+	it("preserves the surrounding environment", () => {
+		let queryEnvironment: NodeJS.ProcessEnv | undefined;
+		const previous = process.env.PRIME_AGENT_PROCESS_START_TEST;
+		process.env.PRIME_AGENT_PROCESS_START_TEST = "preserved";
+		try {
+			getPsProcessStartId(1234, (_command, _args, options) => {
+				queryEnvironment = options?.env;
+				return "Sat Aug  8 06:00:00 2026\n";
+			});
+			expect(queryEnvironment?.PRIME_AGENT_PROCESS_START_TEST).toBe("preserved");
+		} finally {
+			if (previous === undefined) {
+				delete process.env.PRIME_AGENT_PROCESS_START_TEST;
+			} else {
+				process.env.PRIME_AGENT_PROCESS_START_TEST = previous;
+			}
 		}
 	});
 });

JeffCarpenter/prime-agent

packages/coding-agent/src/core/session-lease.ts
@@ -131,17 +131,17 @@ function isProcessAlive(pid: number): boolean {
 	}
 }
 
-type ProcessQuery = (command: string, args: string[]) => string;
+interface ProcessQueryOptions {
+	env?: NodeJS.ProcessEnv;
+}
 
-function runProcessQuery(command: string, args: string[]): string {
+type ProcessQuery = (command: string, args: string[], options?: ProcessQueryOptions) => string;
+
+function runProcessQuery(command: string, args: string[], options?: ProcessQueryOptions): string {
 	return execFileSync(command, args, {
 		encoding: "utf8",
 		stdio: ["ignore", "pipe", "ignore"],
-		// The portable start-time listing renders a local-time timestamp:
-		// without pinning the timezone and locale, the SAME process yields a
-		// different identity when the supervisor restarts under a different
-		// TZ/locale, and the mismatch is then read as PID reuse.
-		env: { ...process.env, TZ: "UTC", LC_ALL: "C" },
+		env: options?.env,
 	});
 }
 
@@ -184,6 +184,22 @@ export function compareProcessStartIds(
 	return recorded.slice(0, recordedSeparator) === observed.slice(0, observedSeparator) ? "mismatch" : "unverifiable";
 }
 
+export function getPsProcessStartId(pid: number, query: ProcessQuery = runProcessQuery): string | undefined {
+	if (!Number.isInteger(pid) || pid <= 0) {
+		return undefined;
+	}
+	try {
+		const startTime = query("ps", ["-p", String(pid), "-o", "lstart="], {
+			env: { ...process.env, TZ: "UTC", LC_ALL: "C" },
+		}).trim();
+		// ps2 distinguishes the stable rendering from legacy ps tokens so an
+		// upgrade cannot mistake a representation change for PID reuse.
+		return startTime ? `ps2:${startTime}` : undefined;
+	} catch {
+		return undefined;
+	}
+}
+
 export function getProcessStartId(pid: number): string | undefined {
 	if (!Number.isInteger(pid) || pid <= 0) {
 		return undefined;
@@ -202,15 +218,7 @@ export function getProcessStartId(pid: number): string | undefined {
 	} catch {
 		// Fall through to the portable process listing used on macOS and BSD.
 	}
-	try {
-		const startTime = runProcessQuery("ps", ["-p", String(pid), "-o", "lstart="]).trim();
-		// ps2: marks the timezone/locale-pinned rendering. Comparisons across
-		// formats (a legacy ps: token recorded by an older build) cannot prove
-		// PID reuse and must degrade to unverifiable instead of mismatch.
-		return startTime ? `ps2:${startTime}` : undefined;
-	} catch {
-		return undefined;
-	}
+	return getPsProcessStartId(pid);
 }
 
 let currentProcessStartId: string | undefined;

packages/coding-agent/test/daemon-ps.test.ts
@@ -15,7 +15,7 @@ import {
 	sortDaemons,
 	verifyHelloSupervisorPid,
 } from "../src/cli/daemon-ps.js";
-import { getProcessStartId } from "../src/core/session-lease.js";
+import { getProcessStartId, getPsProcessStartId } from "../src/core/session-lease.js";
 import { defaultDaemonSocketDir } from "../src/modes/daemon/daemon-socket.js";
 
 describe("worker socket classification", () => {
@@ -112,6 +112,38 @@ describe("verifyHelloSupervisorPid", () => {
 		expect(verifyHelloSupervisorPid(process.pid, processStartId)).toBe(process.pid);
 		if (processStartId) {
 			expect(verifyHelloSupervisorPid(process.pid, `${processStartId}-stale`)).toBeUndefined();
+		}
+	});
+});
+
+describe("getPsProcessStartId", () => {
+	it("uses a stable timezone and locale for the portable process identity", () => {
+		let queryEnvironment: NodeJS.ProcessEnv | undefined;
+		const result = getPsProcessStartId(1234, (_command, _args, options) => {
+			queryEnvironment = options?.env;
+			return "Sat Aug  8 06:00:00 2026\n";
+		});
+
+		expect(result).toBe("ps2:Sat Aug  8 06:00:00 2026");
+		expect(queryEnvironment).toMatchObject({ TZ: "UTC", LC_ALL: "C" });
+	});
+
+	it("preserves the surrounding environment", () => {
+		let queryEnvironment: NodeJS.ProcessEnv | undefined;
+		const previous = process.env.PRIME_AGENT_PROCESS_START_TEST;
+		process.env.PRIME_AGENT_PROCESS_START_TEST = "preserved";
+		try {
+			getPsProcessStartId(1234, (_command, _args, options) => {
+				queryEnvironment = options?.env;
+				return "Sat Aug  8 06:00:00 2026\n";
+			});
+			expect(queryEnvironment?.PRIME_AGENT_PROCESS_START_TEST).toBe("preserved");
+		} finally {
+			if (previous === undefined) {
+				delete process.env.PRIME_AGENT_PROCESS_START_TEST;
+			} else {
+				process.env.PRIME_AGENT_PROCESS_START_TEST = previous;
+			}
 		}
 	});
 });